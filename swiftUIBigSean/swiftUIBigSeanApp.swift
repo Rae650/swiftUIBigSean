d15096b0d0e79c4c11f7756542069db32a7ebf7b
Rae650 <pricemaritza94@yahoo.com>
2023-11-13 10:09:01 -0500

    Initial Commit

---
+++ swiftUIBigSean/swiftUIBigSeanApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIBigSeanApp.swift
+//  swiftUIBigSean
+//
+//  Created by Maritza Price on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIBigSeanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
